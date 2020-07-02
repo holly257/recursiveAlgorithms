c02b74cb57a2baa3632121392b691c4a2051b385
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-02 14:55:33 -0400

    finished strSplit()

--- src/App.js
+++ src/App.js
@@ -43,17 +43,31 @@ function App() {
     }
 
     // 5. String Splitter
-    //in progress
-    function strSplit(string) {
+    function strSplit(string, char) {
         let arr = [];
+
         if (string.length === 0) {
-            return '';
+            return [];
         }
-        if (string[0] == char) {
-            //split into new string in array
+        let charIndex = string.indexOf(char);
+
+        if (charIndex == -1) {
+            return [string];
         }
-        return something + strSplit(str.slice(1));
+        if (charIndex == 0) {
+            return strSplit(string.slice(1), '/');
+        }
+
+        let beginning = string.slice(0, charIndex);
+        let remainingStr = string.slice(charIndex + 1);
+
+        arr = [beginning, ...strSplit(remainingStr, char)];
+        return arr;
     }
+
+    console.log(strSplit('02/20/2020/', '/'));
+
+    //Output: ["02", "20", "2020"]
 
     // 6. Fibonacci
     function fibonacci(a) {
@@ -76,13 +90,40 @@ function App() {
 
     // 8. Find a way out of the maze
     //in progress
-    function solveMaze(array) {
-        if (array == 'e') {
-            return '';
+    function solveMaze(array, currentPos, path) {
+        let row = currentPos[0];
+        let col = currentPos[1];
+
+        if (array[row][col] === 'e') {
+            return path;
         }
 
-        //
+        if (row < array.length - 1) {
+            if (array[row + 1][col] !== '*') {
+                array[row][col] = '*';
+                return solveMaze(array, [row + 1, col], path + 'D');
+            }
+        }
+        if (row > 0) {
+            if (array[row - 1][col] !== '*') {
+                array[row][col] = '*';
+                return solveMaze(array, [row - 1, col], path + 'U');
+            }
+        }
     }
+
+    let mySmallMaze = [
+        [' ', ' ', 'e'],
+        [' ', '*', ' '],
+        [' ', ' ', ' '],
+    ];
+
+    console.log(solveMaze(mySmallMaze, [1, 2], ''));
+
+    //col. array[0].length
+    //no solution - empty
+    //dead end - go back
+
     // 9. Find ALL the ways out of the maze
     // 10. Anagrams
     // 11. Organization Chart
