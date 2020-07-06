12eb40819cce8aa29f02891bfbf18276b4979943
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-07-06 15:16:48 -0400

    solved 9. Find ALL the ways out of the maze

--- src/App.js
+++ src/App.js
@@ -102,6 +102,7 @@ function App() {
                 return solveMaze(array, [row + 1, col], path + 'D');
             }
         }
+        //or = to?
         if (row > 0) {
             if (array[row - 1][col] !== '*') {
                 array[row][col] = '*';
@@ -114,6 +115,7 @@ function App() {
                 return solveMaze(array, [row, col + 1], path + 'R');
             }
         }
+        //or = to?
         if (col > 0) {
             if (array[row][col - 1] !== '*') {
                 array[row][col] = '*';
@@ -131,6 +133,81 @@ function App() {
     console.log(solveMaze(mySmallMaze, [0, 0], ''));
 
     // 9. Find ALL the ways out of the maze
+    let maze = [
+        [' ', ' ', ' ', '*', ' ', ' ', ' '],
+        ['*', '*', ' ', '*', ' ', '*', ' '],
+        [' ', ' ', ' ', ' ', ' ', '*', ' '],
+        [' ', '*', '*', ' ', '*', '*', ' '],
+        [' ', ' ', ' ', ' ', ' ', ' ', 'e'],
+    ];
+    function print(maze) {
+        maze.forEach(row => {
+            console.log(row.join(' '));
+        });
+    }
+    function solveMaze(maze, index) {
+        let paths = [];
+        //print(maze);
+        let row = index[0];
+        let col = index[1];
+        let mazeHeight = maze.length;
+        let mazeWidth = maze[0].length;
+        if ('e' === maze[row][col]) {
+            return [''];
+        }
+        // mark the current spot as visited
+        maze[row][col] = '.';
+        //down
+        let nextRow = row + 1;
+        let nextCol = col;
+        if (
+            nextRow < mazeHeight &&
+            (maze[nextRow][nextCol] === ' ' || maze[nextRow][nextCol] === 'e')
+        ) {
+            let downResult = solveMaze(maze, [nextRow, nextCol]);
+            if (downResult.length != 0) {
+                //We did find a solution
+                downResult.forEach(p => paths.push('D' + p));
+            }
+        }
+        //right
+        nextRow = row;
+        nextCol = col + 1;
+        if (
+            nextCol < mazeWidth &&
+            (maze[nextRow][nextCol] === ' ' || maze[nextRow][nextCol] === 'e')
+        ) {
+            let rightResult = solveMaze(maze, [nextRow, nextCol]);
+            if (rightResult.length !== 0) {
+                //We did find a solution
+                rightResult.forEach(p => paths.push('R' + p));
+            }
+        }
+        //left
+        nextRow = row;
+        nextCol = col - 1;
+        if (nextCol >= 0 && (maze[nextRow][nextCol] === ' ' || maze[nextRow][nextCol] === 'e')) {
+            let leftResult = solveMaze(maze, [nextRow, nextCol]);
+            if (leftResult.length !== 0) {
+                //We did find a solution
+                leftResult.forEach(p => paths.push('L' + p));
+            }
+        }
+        //up
+        nextRow = row - 1;
+        nextCol = col;
+        if (nextRow >= 0 && (maze[nextRow][nextCol] === ' ' || maze[nextRow][nextCol] === 'e')) {
+            let upResult = solveMaze(maze, [nextRow, nextCol]);
+            if (upResult.length !== 0) {
+                //We did find a solution
+                upResult.forEach(p => paths.push('U' + p));
+            }
+        }
+        // if all fails - no directions work
+        maze[row][col] = ' ';
+        return paths;
+    }
+    console.log(solveMaze(maze, [0, 0]));
 
     // 10. Anagrams
     function anagram(string) {
